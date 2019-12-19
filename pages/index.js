42532af87ddd3fe2147291e2bbed54b492426da4
Amar Jasarbasic <amarjasarbasic@gmail.com>
2019-12-19 13:33:32 -0500

    installable greenpack app

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { TextStyle } from '@shopify/polaris';
+
+const Index = () => (
+    <div>
+        <TextStyle variation="positive">
+            Greenpack installed.
+        </TextStyle>
+    </div>
+);
+
+export default Index;
